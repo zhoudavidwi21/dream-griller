79aaa0865a166f65c54b37e4b1db818540d39a83
David Zhou <wi21b026@technikum-wien.at>
2023-06-01 14:11:34 +0200

    created admin area

---
+++ Frontend/sites/admin_area.php
@@ -0,0 +1,35 @@
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-lg-6 mb-4 mt-5">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Add Product</h5>
+                    <a class="btn btn-primary" href="index.php?site=add_product">Produkt hinzufügen</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-lg-6 mb-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Manage Products</h5>
+                    <a class="btn btn-primary" href="index.php?site=manage_products">Produkte verwalten</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-lg-6 mb-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Manage Users</h5>
+                    <a class="btn btn-primary" href="index.php?site=manage_users">Benutzer verwalten</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
