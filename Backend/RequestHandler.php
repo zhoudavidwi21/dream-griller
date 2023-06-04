c774d5ef5fd2ba71c94cb5557efe4e526694d0e3
David <david281100@hotmail.com>
2023-06-04 17:21:48 +0200

    added a RequestHandler basically the API

---
+++ Backend/RequestHandler.php
@@ -0,0 +1,169 @@
+<?php
+
+$requestHandler = new RequestHandler();
+$requestHandler->handleRequest();
+
+class RequestHandler {
+
+    private $userService;
+    private $productService;
+    private $orderService;
+    private $cartService;
+    //...
+
+    public function __construct() {
+        $this->userService = new UserService();
+        $this->productService = new ProductService();
+        $this->orderService = new OrderService();
+        $this->cartService = new CartService();
+    }
+
+    public function handleRequest() {
+        $requestMethod = $_SERVER['REQUEST_METHOD'];
+        $requestUri = $_SERVER['REQUEST_URI'];
+
+        // Extract the resource and additional parameters from the request URI
+        $resource = $this->getResourceFromUri($requestUri);
+        $params = $this->getParamsFromUri($requestUri);
+
+        // Map the HTTP method and resource to the appropriate handler
+        switch ($requestMethod) {
+            case 'GET':
+                $this->handleGetRequest($resource, $params);
+                break;
+            case 'POST':
+                $this->handlePostRequest($resource, $params);
+                break;
+            case 'PUT':
+                $this->handlePutRequest($resource, $params);
+                break;
+            case 'DELETE':
+                $this->handleDeleteRequest($resource, $params);
+                break;
+            default:
+                $this->error(405, "Method not allowed");
+                break;
+        }
+    }
+
+    private function handleGetRequest(string $resource, array $params) {
+        switch ($resource) {
+            case 'users':
+                $this->success($this->userService->getAllUsers());
+                break;
+            case 'user':
+                $this->success($this->userService->getUserByName($params['name']));
+                break;
+            case 'products':
+                $this->success($this->productService->getAllProducts());
+                break;
+            case 'product':
+                $this->success($this->productService->getProductByName($params['name']));
+                break;
+            case 'orders':
+                $this->success($this->orderService->getAllOrders());
+                break;
+            case 'order':
+                $this->success($this->orderService->getOrderById($params['id']));
+                break;
+            default:
+                $this->error(404, "Resource not found");
+                break;
+        }
+    }
+
+    private function handlePostRequest(string $resource, array $params) {
+        switch ($resource) {
+            case 'users':
+                // Handle creating a new user
+                break;
+            case 'products':
+                // Handle creating a new product
+                break;
+            case 'orders':
+                // Handle creating a new order
+                break;
+            default:
+                $this->error(404, "Resource not found");
+                break;
+        }
+    }
+
+    private function handlePutRequest(string $resource, array $params) {
+        switch ($resource) {
+            case 'user':
+                // Handle updating a user
+                break;
+            case 'product':
+                // Handle updating a product
+                break;
+            case 'order':
+                // Handle updating an order
+                break;
+            default:
+                $this->error(404, "Resource not found");
+                break;
+        }
+    }
+
+    private function handleDeleteRequest(string $resource, array $params) {
+        switch ($resource) {
+            case 'user':
+                // Handle deleting a user
+                break;
+            case 'product':
+                // Handle deleting a product
+                break;
+            case 'order':
+                // Handle deleting an order
+                break;
+            default:
+                $this->error(404, "Resource not found");
+                break;
+        }
+    }
+
+    /** format success response and exit
+     * @param int $code HTTP code (2xx)
+     * @param $data result object
+     */
+    private function success($data) {
+        header('Content-Type: application/json');
+        echo(json_encode($data));
+        exit;
+    }
+
+    /** format error (with headers) and exit
+     * @param int $code HTTP response code (4xx or 5xx)
+     * @param string $msg
+     */
+    private function error(int $code, $msg) {
+        http_response_code($code);
+        header('Content-Type: application/json');
+        echo json_encode(['error' => $msg]);
+        exit;
+    }
+
+    private function getResourceFromUri($uri): string {
+        $uriParts = explode('/', $uri);
+        // Resource has to be the first part of uri
+        return $uriParts[1];
+    }
+
+    private function getParamsFromUri($uri): array
+    {
+        $uriParts = explode('/', $uri);
+        $params = [];
+
+        // Extract the parameters from the URI
+        for ($i = 2; $i < count($uriParts); $i += 2) {
+            $params[$uriParts[$i]] = $uriParts[$i + 1];
+        }
+
+        return $params;
+    }
+
+
+
+
+}
