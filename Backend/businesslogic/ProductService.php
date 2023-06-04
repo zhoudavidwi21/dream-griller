9695463cefee9db7327cfeab600a61393abfaede
David <david281100@hotmail.com>
2023-06-04 17:41:05 +0200

    added product service (work in progress still)

---
+++ Backend/businesslogic/ProductService.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * TODO: Implement Product Service Class
+ *  - implements CRUD operations
+ *  - GET products .... get all products
+ *  - GET product ..... get all products by category
+ *  - POST product .... save product (= create new one)
+ *  - DELETE product .. delete existing product
+ */
+class ProductService {
+
+    public function getAllProducts(): array {
+    }
+
+    public function getProductByCategory($category): ?array {
+    }
+
+    public function saveProduct($requestData): ?Product {
+
+    }
+}
