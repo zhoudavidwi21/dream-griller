5b34b23e782a0b1cabc6e7808b7e3d21c0a94b6d
Nico Jöch <wi21b032@technikum-wien.at>
2023-06-09 13:03:01 +0200

    modified cart.js logic with sessionStorage

--- Frontend/js/cart.js
+++ Frontend/js/cart.js
@@ -1,13 +1,21 @@
 $(document).ready(function() {
 
-    if (!sessionStorage.getItem("cart")) {
-        let cart = []
-        sessionStorage.setItem("cart", JSON.stringify(cart))
+    $(document).on('click', '.btnQuant', function(){
+        changeQuant(parseInt($(this).attr("id").slice(11), 10), $(this).text())
+    })
+    $(document).on('click', '[id^=removeProduct]', function(){
+        removeItem(parseInt($(this).attr("id").slice(13), 10))
+    });
+    
+    var cartExists = sessionStorage.getItem("cart")
+
+    if (cartExists) {
+        var globalCart = JSON.parse(cartExists);
+    } else {
+        var globalCart = [];
     }
 
-    //console.log(sessionStorage)
-
-    //on_page_load()
+    
     load_product_data("charcoal", "");                                          //load products for Homepage (default: charcoal, and empty String --> no "search" value)
     load_cart_data();                                                           //load data for cart information
 
@@ -26,31 +34,21 @@ $(document).ready(function() {
 
     })
 
-    /*function on_page_load() {
-        $.ajax({
-            url: '../Backend/RequestHandler.php?resource=products',
-            method: 'GET', // Change to 'POST', 'PUT', 'DELETE' as needed
-            dataType: 'json',
-            success: function(response) {
-                // Handle successful response
-                console.log(response);
-            },
-            error: function(xhr, status, error) {
-                // Handle error
-                console.log(error);
-            }
-        });
-    }*/
-
 
     function load_product_data(categorie, input){
 
         $.ajax({
             
-            url: "../Backend/RequestHandler.php?resource=productCat&params[category]=" +  categorie + "&params[input]=" + input,
+            url: "../Backend/RequestHandler.php",
             method: "GET",
             dataType: "json",
-            //data: {categorie: categorie, input: input},
+            data: {
+                resource: "productCat",
+                params: {
+                  category: categorie,
+                  input: input
+                }
+            },
 
             success: function(response){
 
@@ -61,22 +59,24 @@ $(document).ready(function() {
 
                     let content = `
                     <div class="col-md-4 mb-5">
-                        <div class="card h-100" id="item-">
-                            <div class="card-body">
-                                <a><h2 id="title-${product.id}" class="card-title">${product.name}</h2></a>
-                                <img id="pic-${product.id}" class="img-fluid rounded mb-4 mb-lg-0" src="https://dummyimage.com/200x200/dee2e6/6c757d.png">
-                                <p id="description-${product.id}" class="card-text">${product.description}</p>
-                                <span id="price-${product.id}" class="price fs-4">${product.price} €</span>
-                            </div>
-                            <div class="card-footer">
-                                <a id="prodcart-${product.id}" class="btn btn-sonstige btn-sm">In den Einkaufswagen</a>
-                            </div>
-                        </div>
+                    <div class="card h-100" id="item-">
+                    <div class="card-body">
+                    <a><h2 id="title-${product.id}" class="card-title">${product.name}</h2></a>
+                    <img id="pic-${product.id}" class="img-fluid rounded mb-4 mb-lg-0" src="https://dummyimage.com/200x200/dee2e6/6c757d.png">
+                    <p id="description-${product.id}" class="card-text">${product.description}</p>
+                    <span id="price-${product.id}" class="price fs-4">${product.price} €</span>
+                    </div>
+                    <div class="card-footer">
+                    <a id="prodcart-${product.id}" class="btn btn-sonstige btn-sm">In den Einkaufswagen</a>
+                    </div>
+                    </div>
                     </div>
                     `
 
                     $('#contentRow').append(content);
-                    $('#prodcart-' + product.id).on("click", putInCart)
+                    $('#prodcart-' + product.id).on("click", function(){
+                        putInCart($(this).attr("id").slice(9))
+                    })
                     
                 });
                 
@@ -87,33 +87,8 @@ $(document).ready(function() {
         })
     }
 
-    /*function load_cart_data(){
-
-        $.ajax({
-
-            url: "../Backend/businesslogic/getProductsFromCart.php",                    //selects * products from DB and returns json
-            method: "GET",
-            dataType: "json",
-
-            success:function(data) {
-                $('#cart_details').html(data.tabledata);                                //tabledata is loaded
-                $('#totalCart').html("<b>Gesamtsumme " + data.total + " €</b>");        //total amount is loaded (money to pay)
-                $('#quantity').text(" " + data.count);                                  //overall products count (in Navbar)
-                $("[id^=removeProduct]").on("click", removeItem)                        //removeButtons are enabled
-                $("[id^=btnQuant]").on("click", changeQuant) 
-            },
-            error: function(data){
-                console.log(data);
-            }
-        });
-
-        
-    }*/
 
     function load_cart_data(){
-
-        let cart = JSON.parse(sessionStorage.getItem("cart"))
-        console.log(cart)
 
         let content = `
         <div class="table-responsive" id="order_table">
@@ -129,73 +104,70 @@ $(document).ready(function() {
         let count = 0;
         let overallSum = 0;
 
-        for(i in cart){
-            let item = cart[i];
-
-            let total = item.price * item.quantity;
+        $.each(globalCart, function(key, item) {
+           
+            let total = parseFloat((item.price * item.quantity).toFixed(2));
             count++;
             overallSum += total;
             
             content += `
             <tr>
                 <td>${item.name}</td>
-                <td>${item.quantity} Stück<br>
-                    <button id="btnQuantAdd${item.id}">+</button>
-                    <button id="btnQuantSub${item.id}">-</button>
+                <td id="quantText${item.id}">${item.quantity} Stück<br>
+                    <button id="btnQuantAdd${item.id}" class="btnQuant">+</button>
+                    <button id="btnQuantSub${item.id}" class="btnQuant">-</button>
                 </td>
                 <td>${item.price} €</td>
                 <td>${total} €</td>
                 <td><button class="btn btn-danger btn-xs delete" id="removeProduct${item.id}">Entf.</button></td>
             </tr>
             `
-        }
+        });
 
         content += "</table></div>";
 
         $('#cart_details').html(content);
-        $('#totalCart').html("<b>Gesamtsumme " + overallSum + " €</b>");
+        $('#totalCart').html("<b>Gesamtsumme " + parseFloat(overallSum.toFixed(2)) + " €</b>");
         $('#quantity').text(" " + count);
-        $("[id^=btnQuant]").on("click", changeQuant);
-   
+        
     }
 
 
-    function putInCart(){
-        //über Session noch CartId holen !!
-
-        let id = $(this).attr("id").slice(9)
+    function putInCart(id){
 
         $.ajax({
 
-            url:"../Backend/RequestHandler.php?resource=product&params[id]=" +  id,         //product with related product_id is uploaded to DB
+            url:"../Backend/RequestHandler.php",                                        //product with related product_id is uploaded to DB
             method:"GET",                                              
-            dataType: "json",                    
+            dataType: "json",
+            data: {
+                resource: "product",
+                params: {
+                  id: id
+                }
+              },                 
 
             success:function(data) {
-                let currentCart = JSON.parse(sessionStorage.getItem("cart"));
 
-                let itemExists = currentCart.find(function(item){
+                let itemExists = globalCart.find(function(item){
                     return item.id === data.id;
                 })
 
                 if(itemExists){
                     itemExists.quantity += 1
                 }else{
-                currentCart.push({
-                    id: data.id,
-                    name: data.name,
-                    price: data.price,
-                    quantity: 1
-                })
+                    globalCart.push({
+                        id: data.id,
+                        name: data.name,
+                        price: data.price,
+                        quantity: 1
+                    })
                 }
 
-                sessionStorage.setItem("cart", JSON.stringify(currentCart))
+                sessionStorage.setItem("cart", JSON.stringify(globalCart))
 
+                load_cart_data();                                                       //cart data gets refreshed
 
-                
-                load_cart_data();                                   //cart data gets refreshed
-                /*$('#cartbtn').popover("hide")                           
-                alert(data)*/
             },
             error: function(data){
                 console.log(data);
@@ -207,52 +179,47 @@ $(document).ready(function() {
 
     
 
-    function changeQuant(){
+    function changeQuant(id, method){
 
-        console.log("hello")
-
-        let id = $(this).attr("id").slice(11)
-        let method = $(this).text()                                 //addition or subtraction
-
-        console.log(method)
-
-        let currentCart = JSON.parse(sessionStorage.getItem("cart"));
-        let itemToChange = currentCart.find(function(item){
+        let itemToChange = globalCart.find(function(item){
             return item.id === id;
         })
 
         console.log(itemToChange)
 
         if(method === "+"){
-            itemToChange.quantity++;
+            itemToChange.quantity += 1;
+
+        }else if(itemToChange.quantity === 1){
+            removeItem(itemToChange.id)
+            
         }else{
-            itemToChange.quantity--;
+            itemToChange.quantity -= 1;
         }
 
-        //load_cart_data();
+        sessionStorage.setItem("cart", JSON.stringify(globalCart))
+
+        load_cart_data();
 
 
     }
 
-    function removeItem(){
+    function removeItem(id){
 
-        let id = $(this).attr("id").slice(13)                           //cuts off 13 chars from id, "number" of id remains
+        let itemToChange = globalCart.find(function(item){
+            return item.id === id;
+        })
 
-        $.ajax({
+        console.log(itemToChange)
 
-            url:"../Backend/businesslogic/removeFromCart.php",
-            method:"POST",                                              //cart id is passed and related row gets deleted
-            data: {id: id},                     
+        let index = globalCart.indexOf(itemToChange);
+        if (index > -1) {
+          globalCart.splice(index, 1);
+        }
 
-            success:function(data) {
-                load_cart_data();
-                $('#cartbtn').popover("hide")                           //reload cart data, hide popover and show message to customer
-                alert(data) 
-            },
-            error: function(data){
-                console.log(data);
-            }
-        });
+        sessionStorage.setItem("cart", JSON.stringify(globalCart))
+        
+        load_cart_data();
 
     }
     
