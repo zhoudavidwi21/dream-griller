d848e898c45d2e1ee65b054d8b936064f060bf55
David <david281100@hotmail.com>
2023-06-14 15:57:53 +0200

    formatting, deleted unnecessary files

--- Frontend/res/templates/header.php
+++ Frontend/res/templates/header.php
@@ -85,7 +85,7 @@ ob_start();
               <li><hr class="dropdown-divider"></li>
             <?php } ?>
 
-            <li><a class="dropdown-item" href="../Frontend/res/templates/logout.php">Logout</a></li>
+            <li><a class="dropdown-item" href="#" onclick=logout()>Logout</a></li>
 
           </ul>
         </div>
