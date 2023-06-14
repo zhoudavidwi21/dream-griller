d70a53cfb9804ff3242f344342ee821a845e4ef4
Martin <martin@frischmann.ch>
2023-06-14 04:18:44 +0200

    DB neu

--- Backend/db/alte db/dreamgriller_db (coupuns, producte, usw).sql
+++ Backend/db/alte db/dreamgriller_db (coupuns, producte, usw).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 14. Jun 2023 um 04:17
+-- Erstellungszeit: 12. Jun 2023 um 12:28
 -- Server-Version: 10.4.28-MariaDB
 -- PHP-Version: 8.2.4
 
@@ -34,7 +34,6 @@ CREATE TABLE `coupons` (
   `id` int(11) NOT NULL,
   `code` varchar(100) NOT NULL,
   `amount` double NOT NULL DEFAULT 10,
-  `residual_value` double NOT NULL,
   `expirydate` date NOT NULL,
   `expired` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
@@ -43,10 +42,9 @@ CREATE TABLE `coupons` (
 -- Daten für Tabelle `coupons`
 --
 
-INSERT INTO `coupons` (`id`, `code`, `amount`, `residual_value`, `expirydate`, `expired`) VALUES
-(1, 'testcode123', 10, 10, '2023-06-30', 0),
-(2, '123456', 50, 50, '2023-06-27', 0),
-(3, 'uC65D', 1000, 1000, '2024-06-01', 0);
+INSERT INTO `coupons` (`id`, `code`, `amount`, `expirydate`, `expired`) VALUES
+(1, 'testcode123', 10, '2023-06-30', 0),
+(2, '123456', 50, '2023-06-27', 0);
 
 -- --------------------------------------------------------
 
@@ -77,11 +75,11 @@ CREATE TABLE `customers` (
 --
 
 INSERT INTO `customers` (`id`, `username`, `email`, `password`, `firstname`, `lastname`, `company`, `gender`, `adress`, `postcode`, `city`, `paymethod`, `enabled`, `role`, `logintime`) VALUES
-(1, 'admin', 'admin@admin.com', '$2y$10$sBiZ8m2XuwbBBA97owD3UOPY5e00raQd8LS6XYWxCO0cUcZepBfyi', 'admin', 'admin', 'aaa', 'Herr', 'Ringstraße 1', 1010, 'Wien', 'Vorkasse', 1, 'admin', '2023-06-14 00:42:46'),
-(2, 'test', 'test@test.com', '$2y$10$ifQ7ViSkXFNWTT8Cv3EjYOmOOyxYAdNNkAzya0cexZfZzgzmOjLoG', 'test', 'test', 'sss', 'Frau', 'Hochstädtplatz 1', 1200, 'Wien', 'Kreditkarte', 1, 'user', '2023-06-14 00:43:13'),
-(3, 'as', 'ma@ma.ar', '$2y$10$R4S9OpNT4S7mjWtwpebvpOLhz..X/my84jOw.U5Q6WuPz/smFtY9y', 'ASD', 'a', 'sfa', 'Herr', 'asd', 2, 'Wien', 'Vorkasse', 1, 'user', '2023-06-12 11:07:33'),
-(4, 'asadfsdfsd', 'a@a.wein', '$2y$10$EY3VQbBf27U1LKlVOmFkc.49klt.ZYaTcS7xna.rER35qNq9XWhiK', 'asdf', 'asdf', 'asdf', 'Herr', 'asdf', 222, 'asdf', 'Kreditkarte', 1, 'user', '2023-06-14 00:41:55'),
-(22, 'MF', 'ma@ma.at', '$2y$10$qffvnQVvxur2IYqI6r0sEuLlrPREzfc/MZedRkawGGEles/3GQrba', 'Martin', 'Frischmann', 'Martin EU', 'Firma', 'Ra 23', 2222, 'Wien', 'Kreditkarte', 1, 'user', '2023-06-12 17:11:21'),
+(1, 'admin', 'admin@admin.com', '$2y$10$lklRkv2k3ucGkb8mkzXquOZJMUdBLnHsGp2vc8dguOyCZFWHWS45q', 'admin', 'admin', 'aaa', 'Herr', 'Ringstraße 1', 1010, 'Wien', 'Kreditkarte', 1, 'admin', '2023-06-12 10:04:54'),
+(2, 'test', 'test@test.com', '$2y$10$ifQ7ViSkXFNWTT8Cv3EjYOmOOyxYAdNNkAzya0cexZfZzgzmOjLoG', 'test', 'test', 'sss', 'Frau', 'Hochstädtplatz 1', 1200, 'Wien', 'Kreditkarte', 1, 'user', '2023-06-11 22:48:09'),
+(3, 'as', 'ma@ma.ar', '$2y$10$R4S9OpNT4S7mjWtwpebvpOLhz..X/my84jOw.U5Q6WuPz/smFtY9y', 'ASD', 'a', 'sfa', 'Herr', 'asd', 2, 'Wien', 'Vorkasse', 1, 'user', '2023-06-11 23:04:28'),
+(12, 'asadfsdfsd', 'a@a.wein', '$2y$10$EY3VQbBf27U1LKlVOmFkc.49klt.ZYaTcS7xna.rER35qNq9XWhiK', 'asdf', 'asdf', 'asdf', 'Herr', 'asdf', 222, 'asdf', 'Kreditkarte', 1, 'user', '2023-06-11 19:34:00'),
+(22, 'MF', 'ma@ma.at', '$2y$10$qffvnQVvxur2IYqI6r0sEuLlrPREzfc/MZedRkawGGEles/3GQrba', 'Martin', 'Frischmann', 'Martin EU', 'Firma', 'Ra 23', 2222, 'Wien', 'Kreditkarte', 1, 'user', '2023-06-11 19:34:00'),
 (24, 'sd', 'a@a.asdfsdf', '$2y$10$2c3/NnKH0Jle1mlDCv4.tunDzemwrOQ8dUaceufJpQDoPAI4svCta', 'Martin', 'Frischmann', 'Martin EU', 'Firma', 'asdf 34', 2222, 'Wien', 'Rechnung', 1, 'user', '2023-06-12 10:04:47'),
 (25, 'MJF', 'ma@master.at', '$2y$10$nNlRYNMoc2Ur5eQ9bA7Z.uhNDlmO6hNOSYJ8e2zjwxDkJ3qho13.m', 'Martin', 'Frischmann', 'Martin EU', 'Firma', 'asdf 34', 2222, 'Wien', 'Vorkasse', 1, 'user', '2023-06-11 19:34:00');
 
@@ -130,14 +128,17 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `description`, `price`, `rating`, `image`, `gas`, `charcoal`, `pellet`, `sale`) VALUES
-(1, 'Orlando XXL', 'Erfüllen Sie sich jeden Wunsch beim Grillen mit dem Rost in Rost System des Grillwagens, das nahezu unbegrenzt erweitert werden kann. Auf diese Weise lassen sich leckere Wok-, Gemüse-, und Kartoffelgerichte zubereiten.', 249.99, 4.48, '../Frontend/res/img/products/Orlande XXL.jpg', 0, 1, 0, 0),
-(2, 'Traeger Timberline XL', 'Entdecken Sie die Perfektion des Kochens im Freien mit dem brandneuen Traeger Timberline® Holzpelletgrill -- leistungsstark und extrem vielseitig.', 4399.99, 4.78, '../Frontend/res/img/products/Traeger Timberline XL.jpg', 0, 0, 1, 0),
-(3, 'Rogue SE 525', 'Das limitierte Sondermodell, da bei uns exkl. in Österreich erhältlich ist, ist technisch komplett identisch mit dem Napoleon Rogue SE 525, sieht aber dank seines edlen, Mattschwarzen Designs aus wie ein Phantom. Und das zu einem phantastischen Preis!', 999, 4.50, '../Frontend/res/img/products/Rogue SE 525.jpg', 1, 0, 0, 1),
-(4, 'Crown Pellet 500', 'Von low-and-slow Anbraten bis hin zu high-heat Braten – machen Sie sich bereit für erstaunliche Ergebnisse mit der Bequemlichkeit, der Technologie, der Leistung und dem Geschmack, den Sie mit der Broil King Pellet Grill Serie erhalten.', 1249, 4.90, '../Frontend/res/img/products/Crown Pellet 500.jpg', 0, 0, 1, 0),
-(5, 'Freestyle 365', 'Die neueste Errungenschaft von Napoleon®, der Freestyle! Klein, fein, aber voller Power. Die rechte Seitenablage des Freestyle 365 SIB ist klappbar, so dass dieser Gasgrill extrem kompakt daherkommt. ', 679, 4.25, '../Frontend/res/img/products/Freestyle 365.jpg', 1, 0, 0, 0),
-(6, 'Holzkohle Kugelgrill Ø 57cm', 'Napoleons Holzkohlegrill NK22CK-L mit geschraubten Beinen setzt alles auf die Komfort-Karte: Dank des drehbaren Deckels mit Eckscharnier kann der Grillmeister sicher über der Grillfläche hantieren. Ein großer Aschebehälter aus Stahl und der roboste Kohlerost sind nur weitere Annehmlichkeiten. Auf seinen großen, wetterbeständigen Rädern und mit seinen geschraubten Beinen lässt sich der Rodeo NK22CK-L einfach durchs Gelände ziehen. So ist er immer genau dort, wo Sie grillen wollen! Holzkohle Kugelgrill Ø 57cm', 199, 3.90, '../Frontend/res/img/products/Napoleon Holzkohle Kugelgrill 57cm.jpg', 0, 1, 0, 1),
-(31, 'Moesta Sheriff 6', 'ein Wahnsinns-Griller', 999.99, 4.68, '../Frontend/res/img/products/Moesta Sheriff_6.jpg', 0, 0, 1, 0),
-(32, 'Great Griller', 'Top-Gerät mit allem was das Herz begehrt!!!', 1235.48, 5.00, '../Frontend/res/img/products/6486e3b44fa3e_Griller Fun Pic.jpg', 0, 0, 1, 0);
+(1, 'Orlando XXL', 'Erfüllen Sie sich jeden Wunsch beim Grillen mit dem Rost in Rost System des Grillwagens, das nahezu unbegrenzt erweitert werden kann. Auf diese Weise lassen sich leckere Wok-, Gemüse-, und Kartoffelgerichte zubereiten.', 249.99, 4.48, NULL, 0, 1, 0, 0),
+(2, 'Traeger Timberline XL', 'Entdecken Sie die Perfektion des Kochens im Freien mit dem brandneuen Traeger Timberline® Holzpelletgrill -- leistungsstark und extrem vielseitig.', 4399.99, 5.00, NULL, 0, 0, 1, 0),
+(3, 'Rogue SE 525', 'Das limitierte Sondermodell, da bei uns exkl. in Österreich erhältlich ist, ist technisch komplett identisch mit dem Napoleon Rogue SE 525, sieht aber dank seines edlen, Mattschwarzen Designs aus wie ein Phantom. Und das zu einem phantastischen Preis!', 999, 4.50, NULL, 1, 0, 0, 1),
+(4, 'Crown Pellet 500', 'Von low-and-slow Anbraten bis hin zu high-heat Braten – machen Sie sich bereit für erstaunliche Ergebnisse mit der Bequemlichkeit, der Technologie, der Leistung und dem Geschmack, den Sie mit der Broil King Pellet Grill Serie erhalten.', 1249, 5.00, NULL, 0, 0, 1, 0),
+(5, 'Freestyle 365', 'Die neueste Errungenschaft von Napoleon®, der Freestyle! Klein, fein, aber voller Power. Die rechte Seitenablage des Freestyle 365 SIB ist klappbar, so dass dieser Gasgrill extrem kompakt daherkommt. ', 679, 5.00, NULL, 1, 0, 0, 0),
+(6, 'Holzkohle Kugelgrill Ø 57cm', 'Napoleons Holzkohlegrill NK22CK-L mit geschraubten Beinen setzt alles auf die Komfort-Karte: Dank des drehbaren Deckels mit Eckscharnier kann der Grillmeister sicher über der Grillfläche hantieren. Ein großer Aschebehälter aus Stahl und der roboste Kohlerost sind nur weitere Annehmlichkeiten. Auf seinen großen, wetterbeständigen Rädern und mit seinen geschraubten Beinen lässt sich der Rodeo NK22CK-L einfach durchs Gelände ziehen. So ist er immer genau dort, wo Sie grillen wollen! Holzkohle Kugelgrill Ø 57cm', 199, 5.00, NULL, 0, 1, 0, 1),
+(8, 'Test Griller', 'Der beste Griller der Welt', 9.99, 5.00, NULL, 1, 1, 1, 0),
+(20, 'Griller with image', '', 0, 0.00, NULL, 0, 0, 0, 0),
+(30, 'Griller mit Bild', 'Wow dieser Griller hat sogar ein Bild!', 949.99, 4.50, '../Frontend/res/img/products/6481f8b978031_products-image(1).jpg', 1, 1, 1, 0),
+(31, 'hiho ', 'ein Wahnsinns-Griller', 999.99, 9.99, '../Frontend/res/img/products/64851d7edfd6f_Details_Sheriff_6.jpg', 1, 1, 1, 0),
+(32, 'Great Griller', 'Top-Gerät mit allem was das Herz begehrt!!!', 1235.48, 5.40, '../Frontend/res/img/products/6486e3b44fa3e_Griller Fun Pic.jpg', 0, 0, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -198,7 +199,7 @@ ALTER TABLE `products_orders`
 -- AUTO_INCREMENT für Tabelle `coupons`
 --
 ALTER TABLE `coupons`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT für Tabelle `customers`
